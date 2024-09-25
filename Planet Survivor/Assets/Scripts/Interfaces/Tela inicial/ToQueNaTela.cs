0a7efad2ff905cbc19ddb1a1e5fde4207a435efc
GabrielRang3l <149504041+GabrielRang3l@users.noreply.github.com>
2024-09-24 23:31:51 -0300

    Start scene
    -Start scene em produção

---
+++ Planet Survivor/Assets/Scripts/Interfaces/Tela inicial/ToQueNaTela.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToQueNaTela : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+
+        }
+    }
+}
